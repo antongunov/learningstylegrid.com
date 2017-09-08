6fd91921008914db103e65cdb5c166243daae2b9
Anton Gunov <anton@gunov.name>
2017-09-08 15:51:00 +0700

    Adding Sentence component

---
+++ app/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+    <app-sentence></app-sentence>
+  </div>
+</template>
+
+<script type="text/javascript">
+  import Sentence from './components/Sentence.vue';
+
+  export default {
+    name: 'app',
+    components: {
+      appSentence: Sentence,
+    },
+    data () {
+      return {
+        msg: 'Welcome to Learning Style Grid!'
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ app/components/Sentence.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>{{ begin }}</h1>
+    <ul>
+      <li v-for="end in ends">
+        <p>{{ end }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script type="text/javascript">
+  export default {
+    data () {
+      return {
+        begin: 'Begin sentence...',
+        ends: [
+          '...end (A)',
+          '...end (B)',
+          '...end (C)',
+          '...end (D)',
+        ],
+      };
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
