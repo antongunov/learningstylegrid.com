d3b91f735818659eebbd54617e7855b5878501f3
Anton Gunov <anton@gunov.name>
2017-09-09 22:00:05 +0700

    Added Timer component

--- app/App.vue
+++ app/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <h1>{{ msg }}</h1>
+    <app-timer seconds="300"></app-timer>
     <app-sentence></app-sentence>
   </div>
 </template>
 
 <script type="text/javascript">
   import Sentence from './components/Sentence.vue';
+  import Timer from './components/Timer.vue';
 
   export default {
     name: 'app',
     components: {
       appSentence: Sentence,
+      appTimer: Timer,
     },
     data () {
       return {
---
+++ app/components/Timer.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <p>{{ timerMin }} : {{ timerSec }}</p>
+  </div>
+</template>
+
+<script type="text/javascript">
+  export default {
+    props: {
+      seconds: {
+        type: Number,
+      },
+    },
+    computed: {
+      timerMin() {
+        return ('0' + Math.trunc(this.seconds / 60)).slice(-2);
+      },
+      timerSec() {
+        return ('0' + this.seconds % 60).slice(-2);
+      },
+    },
+    mounted() {
+      setTimeout(() => {
+        const timerId = setInterval(() => {
+          this.seconds -= 1;
+        }, 1000);
+        setTimeout(() => clearInterval(timerId), this.seconds * 1000)
+      }, 1000);
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
