8f270d0e0f02546c6eeaa055f8639176babfc10f
Anton Gunov <anton@gunov.name>
2017-09-20 14:08:16 +0700

    Calc total

--- app/components/Grid.vue
+++ app/components/Grid.vue
@@ -2,7 +2,10 @@
   <div>
     <h1>Grid</h1>
     <ul>
-      <li v-for="rank in ranks">{{ rank }}</li>
+      <li>CE = {{ total[0] }}</li>
+      <li>RO = {{ total[1] }}</li>
+      <li>AC = {{ total[2] }}</li>
+      <li>CE = {{ total[3] }}</li>
     </ul>
     <button @click="onRepeat">Repeat</button>
   </div>
@@ -16,6 +19,18 @@
         type: Array,
         required: true,
       }
+    },
+    computed: {
+      total() {
+        const total = [ 0, 0, 0, 0 ];
+        this.ranks.forEach((rank) => {
+          total[0] += rank[0]; // CE
+          total[1] += rank[1]; // RO
+          total[2] += rank[2]; // AC
+          total[3] += rank[3]; // AE
+        });
+        return total;
+      },
     },
     methods: {
       onRepeat() {
