40303c39814e906c42ee6e8bd35a536892f4fdd2
Anton Gunov <anton@gunov.name>
2017-09-22 17:08:12 +0700

    Support for Vuex

--- app/main.js
+++ app/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue';
 import App from './App.vue';
 
 import { router } from './router'
+import { store } from './store'
 
 new Vue({
   el: '#app',
   router,
-  render: h => h(App)
+  render: h => h(App),
+  store,
 });
